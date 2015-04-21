5ce6d60efd2c5a8057c5b2b63a0d77ba15815cf2
Marc Feldman <mfeldm21@ugrad9.cs.jhu.edu>
2015-04-20 21:08:08 -0400

    DrawPile.h

--- Code/BuildPile.h
+++ Code/BuildPile.h
@@ -8,17 +8,3 @@
  * EN.600.120 Intermediate Programming, Spring 2015
  * SkipBo: DrawPile.h
  */
-
-#ifndef _DRAWPILE_H
-#define _DRAWPILE_H
-
-#include "Pile.h"
-
-class DrawPile : public Pile {
-
-  bool addCard(Card &);
-  Card removeCard();
-
-};
-
-#endif
